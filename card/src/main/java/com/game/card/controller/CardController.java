e8337d59283f66d1aca89423e85c15ac9d253886
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-11 22:13:33 +0400

    CardController created!

---
+++ card/src/main/java/com/game/card/controller/CardController.java
@@ -0,0 +1,9 @@
+package com.game.card.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/card")
+public class CardController {
+}
