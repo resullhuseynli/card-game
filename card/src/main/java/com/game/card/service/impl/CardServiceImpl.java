601394f2f2c58c88b671573b6bf7dc22ef82c8e2
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-11 22:15:24 +0400

    All methods implemented!

--- card/src/main/java/com/game/card/service/impl/CardServiceImpl.java
+++ card/src/main/java/com/game/card/service/impl/CardServiceImpl.java
@@ -1,6 +1,38 @@
 package com.game.card.service.impl;
 
+import com.game.card.model.Card;
 import com.game.card.service.CardService;
 
+import java.util.List;
+
 public class CardServiceImpl implements CardService {
+    @Override
+    public Card getCard(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Card> getCards() {
+        return List.of();
+    }
+
+    @Override
+    public void addCard(Card card) {
+
+    }
+
+    @Override
+    public void deleteCard(long id) {
+
+    }
+
+    @Override
+    public void updateCard(Card card) {
+
+    }
+
+    @Override
+    public List<Card> getCardsByLanguage(String name) {
+        return List.of();
+    }
 }
