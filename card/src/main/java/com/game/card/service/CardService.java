93407d3bacffd68a3cd9147e8bead1c21dcd7c04
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-11 22:29:25 +0400

    getCardsByLanguage method added!

--- card/src/main/java/com/game/card/service/CardService.java
+++ card/src/main/java/com/game/card/service/CardService.java
@@ -1,5 +1,6 @@
 package com.game.card.service;
 
+import com.game.card.enums.Language;
 import com.game.card.model.Card;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,6 @@ public interface CardService {
     void addCard(Card card);
     void deleteCard(long id);
     void updateCard(long id, Card card);
-    List<Card> getCardsByLanguage(String name);
+    List<Card> getCardsByLanguage(Language name);
 
 }
