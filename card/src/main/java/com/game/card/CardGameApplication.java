155071aba9bce49a5e464740f1fcd0b29d247238
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-11 21:37:55 +0400

    @EntityScan added!

--- card/src/main/java/com/game/card/CardGameApplication.java
+++ card/src/main/java/com/game/card/CardGameApplication.java
@@ -2,7 +2,9 @@ package com.game.card;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan
 @SpringBootApplication
 public class CardGameApplication {
 
