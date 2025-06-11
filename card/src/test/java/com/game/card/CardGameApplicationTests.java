08e50441358cf1224daffd141cb88f47a0135752
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-11 21:35:44 +0400

    Git Initialized!

---
+++ card/src/test/java/com/game/card/CardGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
